bc7c9cc4cf908cad15902b4b292d0fbf341aa48f
Issacboox <nurarat0024@gmail.com>
2024-04-03 09:13:50 +0700

    update

--- routes/products.js
+++ routes/products.js
@@ -6,8 +6,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a Product');
 });
 
-router.post('/', function(req, res, next) {
-  res.send('Method Post');
+router.post('/',async function(req, res, next) {
+  try {
+    const { prod_name, price, amount } = req.body;
+    let newProduct = new productModel({
+        prod_name: prod_name,
+        price: price,
+        amount: amount,
+    })
+    let product = await newProduct.save();
+    return res.status(201).send({
+        data:product,
+        message:'product create successfully',
+        success:true,
+    })
+  } catch (error) {
+    return res.status(500).send({
+        message:'error creating product',
+        success:false,
+    })
+  }
 });
 
 router.put('/', function(req, res, next) {
