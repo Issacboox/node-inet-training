b8e904ec6b9dff169dfddf76253f895e6de45a4b
Issacboox <nurarat0024@gmail.com>
2024-04-03 08:56:20 +0700

    k

---
+++ routes/products.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a Product');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('Method Post');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('Method Put');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('Method Delete');
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next) {
+  res.send('Method Post');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('Method Put');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('Method Delete');
+});
+
 module.exports = router;
